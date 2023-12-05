43da0c68f08390f80deaa90948f87498a29a504f
El Mehdi ASSALI <assalielmehdi@gmail.com>
2023-12-05 11:14:51 +0100

    Day 1

---
+++ day1/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+
+	"assalielmehdi/adventofcode2023/util"
+)
+
+func compareSubstr(str string, i int, substr string) bool {
+	if i >= len(str) || i+len(substr) > len(str) {
+		return false
+	}
+
+	ok := true
+
+	for j := 0; ok && j < len(substr); j++ {
+		ok = ok && str[i+j] == substr[j]
+	}
+
+	return ok
+}
+
+func scanNextDigit(str string, i int) int {
+	if i >= len(str) {
+		return -1
+	}
+
+	if unicode.IsDigit(rune(str[i])) {
+		return int(rune(str[i]) - '0')
+	}
+
+	digits := []string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+	for j, dig := range digits {
+		if compareSubstr(str, i, dig) {
+			return j
+		}
+	}
+
+	return -1
+}
+
+func calculateCalibration(line string) int {
+	firstDig, lastDig := -1, -1
+
+	for i := range line {
+		dig := scanNextDigit(line, i)
+
+		if dig != -1 {
+			if firstDig == -1 {
+				firstDig = dig
+			}
+
+			lastDig = dig
+		}
+	}
+
+	return firstDig*10 + lastDig
+}
+
+func main() {
+	lines := util.NewFileIterator("day1/input2.txt")
+	defer lines.Close()
+
+	sum := 0
+
+	for lines.HasNext() {
+		line := lines.Next()
+		sum += calculateCalibration(line)
+	}
+
+	fmt.Println(sum)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module assalielmehdi/adventofcode2023
+
+go 1.21.4
